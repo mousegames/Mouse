96ae53d9957776ac375d52f6703ffe813b57157f
mousegames <stav.kowadlo@gmail.com>
2024-08-02 17:01:08 +1000

    Meow 1.01

--- mouse/main.py
+++ mouse/main.py
@@ -2,8 +2,26 @@ main = []
 
 while True:
     line = input('')
-    if line == 'add:':
+    if line == 'add: s':
         main.append(input(''))
+    elif line == 'add: b':
+        main.append(bool(input('')))
+    elif line == 'add: i':
+        main.append(int(input('')))
+    elif line == 'add: f':
+         main.append(float(input('')))
+    elif line == 'add:':
+            add = main[int(input(''))] + main[int(input(''))]
+            print(f'---{add}---')
+    elif line == 'mult:':
+        mult = main[int(input(''))] * main[int(input(''))]
+        print(f'---{mult}---')
+    elif line == 'sub:':
+        sub = main[int(input(''))] - main[int(input(''))]
+        print(f'---{sub}---')
+    elif line == 'div:':
+        div = main[int(input(''))] / main[int(input(''))]
+        print(f'---{div}---')
     elif line == 'delete:':
         main.pop(int(input('')))
     elif line == 'pr:':
